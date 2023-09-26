65855f9b7e780180e11a27d4d42df400ffca86f4
Roma Taranto <rtaranto@smith.edu>
2023-09-26 18:31:26 -0400

    Added comments and docstrings to both client and server

--- echo_client.py
+++ echo_client.py
@@ -1,13 +1,30 @@
+"""
+This program works in tandem with a server. It is a client designed to prompt the user 
+for two numbers and an operation to perform. The operation options include finding 
+the LCM or the mean of the two provided numbers. Once sent to the server, the
+client receives a response from the server and presents this to the user.
+
+"""
+# low-level networking interface library
 import socket
 
 HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 65432  # The port used by the server
+
+# prompting the user for two numbers and to choose an operation
 numbers = input("give two numbers, separated by a comma: ")
 operation = input("would you like the server to find the LCM or the mean? ")
+
+# sets the deliminter to be a semi-colon, useful to separate the user inputs
+#   so the server knows where to look for the numbers/operation
 delimiter = ";"
 MSG = operation + delimiter + numbers
 
+# tracing to let the user know that the client is attempting connection to the server
 print("client starting - connecting to server at IP", HOST, "and port", PORT)
+
+# once connected, the client converts the message to bytes and sends it to the server
+# tracing lets the user know once the message has been sent
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     print(f"connection established, sending message '{MSG}'")
@@ -15,5 +32,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     print("message sent, waiting for reply")
     data = s.recv(1024)
 
+# after the server has sent the response, the client converts back from bytes to
+#   a string and displays this to the user
 print(f"received response: {data.decode('utf-8')} [{len(data)} bytes]")
 print("client is done!")
--- echo_server.py
+++ echo_server.py
@@ -1,12 +1,24 @@
+"""
+This program works in tadem with a client. It is a server designed to accept two
+numbers and an operation from the client. With this information, the server will
+perform the operation using the provided numbers and return the result to the
+client.
+
+"""
+# necessary libraries
 import socket
 import math
 
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 
+# the chosen delimiter on the client side, needed to decode the data
 delimiter = ";"
 
+# tracing to let the  user know that the server is attempting connection with the client
 print("server starting - listening for connections at IP", HOST, "and port", PORT)
+
+# once connected, tracing lets the user know
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
@@ -18,37 +30,55 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             if not data:
                 break
             
-            decoded_data = data.decode('utf-8') #decoding incoming data from bytes to a string
+            # decoding incoming data from bytes to a string
+            decoded_data = data.decode('utf-8') 
 
+            # tracing to let the user know what the received data is
             print("received client message: " + decoded_data)
 
+            # separating the operation and numbers based on the delimiter
             operation = decoded_data.split(delimiter)[0]
             both_numbers = decoded_data.split(delimiter)[1]
 
+            # tracing to let the user know what the operation and numbers are
             print("requested operation is " + operation)
             print("requested arguments: " + both_numbers)
-            #print("computing " + operation + " of " + both_numbers)
 
+            # separating the numbers with a comma as the delimiter
             first_num = int(both_numbers.split(",")[0])
             second_num = int(both_numbers.split(",")[1])
 
+            # tracing to let the user know what the server is performing
             print(f"computing {operation} of {both_numbers}")
 
+            # series of if statements that will excecute based on operation
+            # note useage of f-strings for improved readability
             if operation == "LCM":
+                # uses math library's built in LCM function
                 LCM = math.lcm(first_num,second_num)
-                #print("The LCM of " + str(first_num) + " and " + str(second_num) + " is " + str(LCM))
+
+                # tracing to let the user know the result
                 print(f"The LCM of {first_num} and {second_num} is {LCM}")
                 print("sending result back to client")
+
+                # converts integer to bytes and sends back to client
                 conn.sendall(bytes(str(LCM), 'utf-8'))
             elif operation == "mean":
+                # basic calculation for mean
                 mean = (first_num + second_num) / 2
-                #print("The mean of " + str(first_num) + " and " + str(second_num) + " is " + str(mean))
+
+                # tracing to let the user know the result
                 print(f"The mean of {first_num} and {second_num} is {mean}")
                 print("sending result back to client")
+
+                # converts integer to bytes and sends back to client
                 conn.sendall(bytes(str(mean), 'utf-8'))
             else:
+                # if the given operation is neither LCM nor mean, there is an error
                 print("ERROR: Invalid operation")
                 print("sending result back to client")
+
+                # sends error message back to the client
                 conn.sendall("ERROR: Invalid operation")
 
 print("server is done!")
